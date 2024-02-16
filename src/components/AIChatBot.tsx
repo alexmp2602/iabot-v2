b4ddebeb871c363bb2c960038bc3428ee2e53b48
Alex Pereyra <alexmp.2602@gmail.com>
2024-02-16 13:56:27 -0300

    Agregadas dependencias de paquetes y actualizadas las versiones de algunas dependencias en package.json

---
+++ src/components/AIChatBot.tsx
@@ -0,0 +1,20 @@
+import {useChat} from "ai/react";
+
+interface AiChatBotProps {
+    open: boolean;
+    onClose: () => void
+}
+
+export default function AiChatBot({open, onClose}: AiChatBotProps) {
+    const {
+        messages,
+        input,
+        handleInputChange,
+        handleSubmit,
+        setMessages,
+        isLoading,
+        error
+    } = useChat(); // /api/chat
+
+    return <div className={cn("bottom-0 right-0 z-50 w-full max-w-[500px] p-1 xl:right-36")}></div>
+}
