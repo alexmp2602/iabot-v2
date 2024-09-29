5d5752454c971b99eb6797cc17a5da34a55c73c3
alexmp.2602 <alexmp.2602@gmail.com>
2024-09-29 00:32:20 -0300

    Actualizar importaciones de estilos y agregar dependencia de react-icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-"use client";
-
-import { Metadata } from 'next';
+import { Metadata } from "next";
 import "@/app/globals.css";
-import Head from 'next/head';
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "IABOT - Robótica Educativa y Capacitaciones para Docentes",
-  description: "IABOT es una empresa dedicada a la robótica educativa y a la capacitación de docentes.",
-  keywords: "robótica, educación, capacitación, docentes, IABOT, LEGO, SPIKE, Prime, EV3, WeDo, cursos de robótica para docentes, kits de robótica educativa, capacitación en robótica educativa",
+  description:
+    "IABOT es una empresa dedicada a la robótica educativa y a la capacitación de docentes.",
+  keywords:
+    "robótica, educación, capacitación, docentes, IABOT, LEGO, SPIKE, Prime, EV3, WeDo, cursos de robótica para docentes, kits de robótica educativa, capacitación en robótica educativa",
   authors: [{ name: "IABOT Team", url: "https://www.iabot.com.ar" }],
 };
 
@@ -16,7 +16,9 @@ interface RootLayoutProps {
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-  const keywords = Array.isArray(metadata.keywords) ? metadata.keywords.join(', ') : metadata.keywords ?? '';
+  const keywords = Array.isArray(metadata.keywords)
+    ? metadata.keywords.join(", ")
+    : (metadata.keywords ?? "");
 
   return (
     <>
@@ -24,20 +26,21 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bruno+Ace&family=Avenir+LT+Std:wght@400&display=swap" />
-        <meta name="description" content={metadata.description ?? ''} />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Bruno+Ace&family=Avenir+LT+Std:wght@400&display=swap"
+        />
+        <meta name="description" content={metadata.description ?? ""} />
         <meta name="keywords" content={keywords} />
-        <meta property="og:title" content={String(metadata.title) ?? ''} />
-        <meta property="og:description" content={metadata.description ?? ''} />
+        <meta property="og:title" content={String(metadata.title) ?? ""} />
+        <meta property="og:description" content={metadata.description ?? ""} />
         <meta property="og:url" content="https://www.iabot.com.ar" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/assets/img/logo/JPG/iabot.jpg" />
       </Head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </>
   );
-}
+};
 
 export default RootLayout;
