e2979f9a10d9b2ac4eea18d3398631dd2b5591e4
alexmp.2602 <alexmp.2602@gmail.com>
2024-10-12 18:08:42 -0300

    Agregar configuración de patrones remotos para imágenes de Instagram en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
-export const images = {
+// next.config.mjs
+const nextConfig = {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'www.instagram.com',
+        port: '',
         pathname: '/p/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'scontent.cdninstagram.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
-  };
-  
+  },
+};
+
+export default nextConfig;
