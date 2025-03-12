b9f322458bcc378a21fb6cfc62b85e8f5fe8cb6b
alexmp.2602 <alexmp.2602@gmail.com>
2025-03-12 16:50:09 -0300

    Eliminar imágenes no utilizadas de marcas y mejorar la animación del componente TrustedBrands

--- src/components/TrustedBrands.tsx
+++ src/components/TrustedBrands.tsx
@@ -4,39 +4,40 @@ import Image from 'next/image';
 
 export default function TrustedBrands() {
   const brands = [
-    { src: '/assets/img/marcas/swiss-medical-logo.png', alt: 'Swiss Medical' },
     {
       src: '/assets/img/marcas/colegio-los-robles-logo.png',
       alt: 'Colegio Los Robles',
     },
-    { src: '/assets/img/marcas/sm-gusta-logo.jpg', alt: 'SM Gusta' },
-    { src: '/assets/img/marcas/mas-cerca-logo.jpg', alt: 'Más Cerca' },
-    { src: '/assets/img/marcas/red-full-logo.jpg', alt: 'Red Full' },
     { src: '/assets/img/marcas/colegio-sm.png', alt: 'Colegio Santa María' },
-    { src: '/assets/img/marcas/on-axion.jpg', alt: 'On Axion Energy' },
+    { src: '/assets/img/marcas/swiss-medical-logo.png', alt: 'Swiss Medical' },
     { src: '/assets/img/marcas/medife.png', alt: 'Medife' },
+    { src: '/assets/img/marcas/shell-box.png', alt: 'Shell' },
+    { src: '/assets/img/marcas/axion-energy.jpg', alt: 'Axion Energy' },
   ];
 
   return (
-    <div className="overflow-hidden">
-      <div className="relative w-full overflow-hidden">
-        <div className="flex w-max animate-marquee space-x-8">
-          {brands.concat(brands).map((brand, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-center mx-6"
-              aria-label={`Logo de ${brand.alt}`}
-            >
-              <Image
-                src={brand.src}
-                alt={`Logo de ${brand.alt}`}
-                width={120}
-                height={120}
-                priority={false}
-                className="object-contain rounded-lg"
-              />
-            </div>
-          ))}
+    <div className="overflow-hidden w-full py-6 bg-white">
+      <div className="relative flex w-full whitespace-nowrap">
+        <div className="flex items-center justify-center space-x-6 animate-marquee">
+          {Array(4)
+            .fill(brands)
+            .flatMap((arr) => arr)
+            .map((brand, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-center mx-6"
+                aria-hidden={index >= brands.length}
+              >
+                <Image
+                  src={brand.src}
+                  alt={brand.alt}
+                  width={120}
+                  height={120}
+                  priority={false}
+                  className="object-contain rounded-lg"
+                />
+              </div>
+            ))}
         </div>
       </div>
     </div>
